d4a97290d638fc8d78732c86200ed5aca3f308e7
Hadi Hassoun <hassounhadi8@gmail.com>
2025-10-15 16:49:50 +0300

    Adding navbar component

---
+++ components/navbar-04/logo.tsx
@@ -0,0 +1,28 @@
+export const Logo = () => (
+  <div className="flex items-center gap-2">
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 16 L8 16 L10 12 L14 20 L18 10 L22 16 L30 16"
+        stroke="#10b981" // Primary Blue
+        strokeWidth="2"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle cx="30" cy="16" r="2" fill="#F59E0B" /> // Accent Amber
+    </svg>
+
+    <span
+      className="text-2xl font-bold"
+      style={{ fontFamily: "Inter, sans-serif", color: "#3b82f6" }}
+    >
+      FitTrack
+    </span>
+  </div>
+);
---
+++ components/navbar-04/navbar-04.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { NavigationSheet } from "./navigation-sheet";
+
+const Navbar = () => {
+  return (
+    <div className="min-h-screen bg-muted">
+      <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-(--breakpoint-xl) mx-auto rounded-full">
+        <div className="h-full flex items-center justify-between mx-auto px-4">
+          <Logo />
+
+          {/* Desktop Menu */}
+          {/* <NavMenu className="hidden md:block" /> */}
+
+          <div className="flex items-center gap-3">
+            <Button
+              variant="secondary"
+              className="hidden sm:inline-flex rounded-full"
+            >
+              Sign In
+            </Button>
+            <Button className="rounded-full">Get Started</Button>
+
+            {/* Mobile Menu */}
+            <div className="md:hidden">
+              <NavigationSheet />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
